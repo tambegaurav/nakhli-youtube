bb586182e7f76b063b81fb217feaab695c4056ee
tambegaurav <gauravkishortambe@gmail.com>
2021-01-04 01:43:27 +0530

    add login, setup react-router, add redux

--- src/App.js
+++ src/App.js
@@ -6,22 +6,53 @@ import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import "./_app.scss";
 import LoginScreen from "./screens/LoginScreen/LoginScreen";
 
-const App = () => {
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+const Layout = ({ children }) => {
   const [sidebar, toggleSidebar] = useState(false);
 
   const handleToggleSidebar = () => toggleSidebar((value) => !value);
 
   return (
-    // <div>
-    //   <Header handleToggleSidebar={handleToggleSidebar} />
-    //   <div className="app_container">
-    //     <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
-    //     <Container fluid className="app_main">
-    //       <HomeScreen />
-    //     </Container>
-    //   </div>
-    // </div>
-    <LoginScreen />
+    <>
+      <Header handleToggleSidebar={handleToggleSidebar} />
+      <div className="app_container">
+        <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
+        <Container fluid className="app_main">
+          {children}
+        </Container>
+      </div>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Layout>
+            <HomeScreen />
+          </Layout>
+        </Route>
+        <Route path="/login">
+          <LoginScreen />
+        </Route>
+        <Route path="/search">
+          <Layout>
+            <h1>Search Results</h1>
+          </Layout>
+        </Route>
+        <Route>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0JPd29xDzMYd1R1WY169dXZJpFIWhduk",
+  authDomain: "youuuuuutubeee.firebaseapp.com",
+  projectId: "youuuuuutubeee",
+  storageBucket: "youuuuuutubeee.appspot.com",
+  messagingSenderId: "698845645223",
+  appId: "1:698845645223:web:65764adcd67c827dc10e82",
+};
+
+firebase.initializeApp(firebaseConfig);
